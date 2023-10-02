3c3e84381d219c4073583d019d85887f75eb905f
silasbrasil <silas.swf@gmail.com>
2023-10-02 08:59:49 -0300

    add nginx and docker-compose

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,4 @@ FROM node:18.12.0-alpine
 COPY . .
 RUN npm ci
 
-RUN npm install -g forever
-
-CMD ["forever", "-f", "api.js"]
+CMD ["node", "api.js"]
--- api.js
+++ api.js
@@ -2,16 +2,16 @@ import express from 'express';
 import { createPerson, getPersonById, searchPerson, countPerson } from './src/index.js';
 
 const app = express()
-const port = 3000
+const port = 8080
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.post('/pessoas', createPerson)
-app.get('/pessoas/:id', getPersonById)
 app.get('/pessoas', searchPerson)
+app.get('/pessoas/:id', getPersonById)
 app.get('/contagem-pessoas', countPerson)
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Api listening on port ${port}`)
 })
